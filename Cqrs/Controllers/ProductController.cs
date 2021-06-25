bd6f14ad17ecf094f11724a161d9df8f6fbb5be8
Emre Toraman <emretoraman@gmail.com>
2021-06-25 16:11:26 +0300

    product controller

---
+++ Cqrs/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Cqrs.Features.ProductFeatures.Commands;
+using Cqrs.Features.ProductFeatures.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Cqrs.Controllers
+{
+    public class ProductController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllProductsQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await Mediator.Send(new GetProductByIdQuery { Id = id }));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateProductCommand command)
+        {
+            if (id != command.Id) return BadRequest();
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new DeleteProductCommand { Id = id }));
+        }
+    }
+}
