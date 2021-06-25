3649f95b0c1d066aecdd062fa075e4b844216017
Emre Toraman <emretoraman@gmail.com>
2021-06-25 07:42:45 +0300

    context

---
+++ Cqrs/Context/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Cqrs.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Cqrs.Context
+{
+    public class ApplicationContext : DbContext, IApplicationContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        { }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ Cqrs/Context/IApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Cqrs.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Cqrs.Context
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
